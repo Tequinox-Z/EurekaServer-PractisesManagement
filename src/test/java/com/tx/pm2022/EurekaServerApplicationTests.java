3ba09f10b1dcd3ac1b0e3528da8187d3a108aee1
tequinox <tequinox@unknown>
2022-05-18 21:57:12 +0200

    Versión local de Eureka Server

---
+++ src/test/java/com/tx/pm2022/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tx.pm2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
